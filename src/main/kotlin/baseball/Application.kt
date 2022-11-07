a946eb10878b15c4b28275066ab8b45ecc22e8a5
Jtility <leejewon97@gmail.com>
2022-11-08 03:47:27 +0900

    feat: make makeRandomBaseballNumber
    make makeRandomBaseballNumber that return List<String>

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -1,7 +1,19 @@
 package baseball
 
+import camp.nextstep.edu.missionutils.Randoms
+
 fun main() {
 //	TODO("프로그램 구현")
 	println("숫자 야구 게임을 시작합니다.")
-	var answer = makeRandomBaseballNumber()
+	val answer = makeRandomBaseballNumber()
+}
+
+fun makeRandomBaseballNumber(): List<String> {
+	val numbers = mutableListOf<String>()
+	while (numbers.size < 3) {
+		val number = Randoms.pickNumberInRange(1, 9).toString()
+		if (number != "0")
+			numbers.add(number)
+	}
+	return numbers
 }
